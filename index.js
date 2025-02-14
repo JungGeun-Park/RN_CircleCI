19cc7e8c2b1c20c6b207f6e252850cb114cb189f
puzznic <puzznic@inka.co.kr>
2025-02-14 09:47:19 +0900

    int

--- index.js
+++ index.js
@@ -7,3 +7,89 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+//############################################################## AppSealing Code-Part BEGIN: DO NOT MODIFY THIS LINE !!!
+import {Alert, NativeModules} from 'react-native';
+
+const DETECTED_JAILBROKEN = 0x00000001;
+const DETECTED_DRM_DECRYPTED = 0x00000002;
+const DETECTED_DEBUG_ATTACHED = 0x00000004;
+const DETECTED_HASH_INFO_CORRUPTED = 0x00000008;
+const DETECTED_CODESIGN_CORRUPTED = 0x00000010;
+const DETECTED_HASH_MODIFIED = 0x00000020;
+const DETECTED_EXECUTABLE_CORRUPTED = 0x00000040;
+const DETECTED_CERTIFICATE_CHANGED = 0x00000080;
+const DETECTED_BLACKLIST_CORRUPTED = 0x00000100;
+const DETECTED_CHEAT_TOOL = 0x00000200;
+try
+{
+ const {AppSealingInterfaceBridge} = NativeModules;
+ let security_threat = parseInt( AppSealingInterfaceBridge.IsAbnormalEnvironmentDetectedRN() );
+ if ( security_threat > 0 )
+ {
+ // Show GUI
+ let msg = "\n--------------------------------------"
+ + "\nAbnormal Environment Detected !!"
+ + "\n--------------------------------------\n";
+ if (( security_threat & DETECTED_JAILBROKEN ) > 0 )
+ msg += "\n - Jailbroken";
+ if (( security_threat & DETECTED_DRM_DECRYPTED ) > 0 )
+ msg += "\n - Executable is not encrypted";
+ if (( security_threat & DETECTED_DEBUG_ATTACHED ) > 0 )
+ msg += "\n - App is debugged";
+ if (( security_threat & DETECTED_HASH_INFO_CORRUPTED ) > 0 || ( security_threat & DETECTED_HASH_MODIFIED ) > 0 )
+ msg += "\n - App integrity has broken";
+ if (( security_threat & DETECTED_CODESIGN_CORRUPTED ) > 0 || ( security_threat & DETECTED_EXECUTABLE_CORRUPTED ) > 0 )
+ msg += "\n - App executable has corrupted";
+ if (( security_threat & DETECTED_CERTIFICATE_CHANGED ) > 0 )
+ msg += "\n - App has re-signed";
+ if (( security_threat & DETECTED_BLACKLIST_CORRUPTED ) > 0 )
+ msg += "\n - Blacklist/whitelist has corrupted";
+ if (( security_threat & DETECTED_CHEAT_TOOL ) > 0 )
+ msg += "\n - Cheat tool has detected";
+ console.log( "AppSealing Security Threat: " + msg );
+ Alert.alert( "AppSealing Security Threat", msg,
+ [{ text: "Confirm", onPress: () => { AppSealingInterfaceBridge.ExitApp(); }}], { cancelable: false } );
+ }
+}
+catch( e )
+{
+ console.log( "### AppSealing Security Check : " + e.ToString() );
+}
+
+AppSealingPeriodicAlerted = false;
+function AppSealingPeriodicCheck() {
+ if( AppSealingPeriodicAlerted ){
+ return;
+ }
+ try
+ {
+ const {AppSealingInterfaceBridge} = NativeModules;
+ let ret = parseInt( AppSealingInterfaceBridge.IsSwizzlingDetectedReturnRN() );
+
+ if ( ret >= 1 && ret <= 3 )
+ {
+ // Show GUI
+ let msg = "\n--------------------------------------"
+ + "\nAbnormal Environment Detected !!"
+ + "\n--------------------------------------\n";
+ if ( ret == 1 )
+ msg += "\n - Jailbroken";
+ if ( ret == 2 )
+ msg += "\n - Method Swizzling";
+ if ( ret == 3 )
+ msg += "\n - Method Hooking";
+
+ console.log( "AppSealing Security Threat: " + msg );
+ Alert.alert( "AppSealing Security Threat", msg, [{ text: "Confirm", onPress: () => { AppSealingInterfaceBridge.ExitApp(); }}],
+{ cancelable: false } );
+ AppSealingPeriodicAlerted = true;
+ }
+ }
+ catch( e )
+ {
+ console.log( "### AppSealing Security Check : " + e.ToString() );
+ }
+}
+setInterval(AppSealingPeriodicCheck, 2500); // 2.5ms
+//############################################################## AppSealing Code-Part END: DO NOT MODIFY THIS LINE !!!
--- ios/AiSM_RN075/AppDelegate.mm
+++ ios/AiSM_RN075/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "AppsealingiOS.h"
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -10,6 +11,51 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+
+  //############################################################## AppSealing Code-Part BEGIN: DO NOT MODIFY THIS LINE !!!
+  #if true  //--------------------------------------- APPSEALING-GetDeviceID [BEGIN] : DO NOT REMOVE THIS COMMENT !!!
+  NSString* _appsealing_msg1 = @"\n\n* AppSealing Device ID : ";
+  char _appSealingDeviceID_auto_generated[64];
+  
+  // query AppSealing device unique identifier (optional)
+  if ( ObjC_GetAppSealingDeviceID( _appSealingDeviceID_auto_generated ) == 0 )
+      _appsealing_msg1 = [_appsealing_msg1 stringByAppendingString:[[NSString alloc] initWithUTF8String:_appSealingDeviceID_auto_generated]];
+  else
+      _appsealing_msg1 = [_appsealing_msg1 stringByAppendingString:@"Unknown"];
+  NSLog( [_appsealing_msg1 stringByAppendingString:@"%s"], "\n" );
+  #endif    //--------------------------------------- APPSEALING-GetDeviceID [END] : DO NOT REMOVE THIS COMMENT !!!
+
+  #if true  //--------------------------------------- APPSEALING-GetCredential [BEGIN] : DO NOT REMOVE THIS COMMENT !!!
+  NSString* _appsealing_msg3 = @"\n\n* AppSealing Credential : ";
+  char _appSealingCredential_auto_generated[290];
+  if ( ObjC_GetEncryptedCredential( _appSealingCredential_auto_generated ) == 0 )
+      _appsealing_msg3 = [_appsealing_msg3 stringByAppendingString:[[NSString alloc] initWithUTF8String:_appSealingCredential_auto_generated]];
+  else
+      _appsealing_msg3 = [_appsealing_msg3 stringByAppendingString:@"Unknown"];
+  NSLog( [_appsealing_msg3 stringByAppendingString:@"%s"], "\n" );
+  // use thie credential value in your authentication function
+  #endif    //--------------------------------------- APPSEALING-GetCredential [END] : DO NOT REMOVE THIS COMMENT !!!
+
+  #if false //--------------------------------------- APPSEALING-AntiSwizzling [BEGIN] : DO NOT REMOVE THIS COMMENT !!!
+  [AppSealingInterface _NotifySwizzlingDetected:^( NSString* msg ) {
+      UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"AppSealing"
+                                  message:msg
+                                  preferredStyle:UIAlertControllerStyleAlert];
+      UIAlertAction *confirm = [UIAlertAction actionWithTitle:@"Confirm"
+                                  style:UIAlertActionStyleDefault
+                                  handler:^( UIAlertAction * _Nonnull action ) {
+                              #if !DEBUG && !defined(DEBUG) // Debug mode does not kill app even if security threat has found
+                                      exit( 0 );
+                              #endif
+                                  }];
+      [alert addAction:confirm];
+      [self presentViewController:alert animated:YES completion:nil];
+  }];
+  #endif    //--------------------------------------- APPSEALING-AntiSwizzling [END] : DO NOT REMOVE THIS COMMENT !!!
+  //############################################################## AppSealing Code-Part END: DO NOT MODIFY THIS LINE !!!
+
+
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
